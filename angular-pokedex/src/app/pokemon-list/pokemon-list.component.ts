a582ba441ee465ec56343741e37c29a0c2e86c0a
vitoria-vieirac <114622524+vitoria-vieirac@users.noreply.github.com>
2023-10-20 07:15:38 -0300

    first commit

---
+++ angular-pokedex/src/app/pokemon-list/pokemon-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+import { PokemonService } from '../services/pokemon.service';
+
+@Component({
+  selector: 'app-pokemon-list',
+  templateUrl: './pokemon-list.component.html',
+  styleUrls: ['./pokemon-list.component.sass'],
+
+})
+export class PokemonListComponent {
+
+  constructor(public pokemonService: PokemonService) {}
+
+}
+
